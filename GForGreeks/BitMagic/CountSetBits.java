60ee627e601ea1d9c80d278c3b1dbaaf57acd260
Sai Sumanth Reddy - Mac <tssumanth18@gmail.com>
2022-04-09 16:49:27 +0530

    bit magic

---
+++ GForGreeks/BitMagic/CountSetBits.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package BitMagic;
+
+/**
+ * @author saisumanthreddytalamanchi
+ *
+ */
+public class CountSetBits {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println(func_usingBitWise(5));
+
+	}
+	
+	static int func(int n)
+	{
+		int res =0;
+		
+		while(n>0)
+		{
+			if(n%2 !=0)
+				res++;
+			n=n/2;
+		}
+		
+		return res;
+	}
+	
+	static int func_usingBitWise(int n)
+	{
+		int res =0;
+		
+		while(n>0)
+		{
+			if((n&1) == 1)
+				res++;
+			n=n >> 1;
+			System.out.println(n);
+		}
+		
+		return res;
+	}
+	
+
+}
